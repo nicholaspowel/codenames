fbfcd36f8aee55a153de7fc13359e90588eb83e2
Nick <nicholaspowel@users.noreply.github.com>
2017-08-12 18:00:15 -0400

    spymaster effect almost fully implemented

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -9,21 +9,39 @@ class ListItem extends React.Component {
   }
   
 
-render(){
-
-  const styleObj = {
-    background: "black",
+  toggleType(){
+    if( displayObj.display==='none'){
+      displayObj.display = 'flex';
+    } else {
+      displayObj.display = 'none';
+    }
   }
-  if(this.props.tile.isClicked){
-    styleObj.background = this.props.color;
-  }
-    return(
-      <div className='tile' style={styleObj} onClick={(e) => this.props.Utilities.tileClick(this.props.tile)}>
-        <div className='word'>
-          { this.props.tile.word }
-        </div>
-      </div>      
-  	)}
-}
 
-export default ListItem;
+  render() {
+    var displayObj= {
+      display: "none"
+    };
+    var styleObj = {
+      background: "black",
+    };
+    var classObj = 'tile';
+
+    if(this.props.tile.display){
+      classObj = 'tile ' + this.props.tile.color;
+    }
+      return(
+        <div className={classObj} style={styleObj} onClick={(e) => this.props.Utilities.tileClick(this.props.tile)}>
+          <div className='word'>
+            { this.props.tile.word }
+          </div>
+          <button className="btn hidden-sm-down" style={{zIndex: 2}}onClick={(e)=> this.toggleType.bind(this)}>
+            <span className="glyphicon glyphicon-search"></span>
+          </button>
+          <div className='customWord'>
+            <input className="form-control" style={displayObj} type="text" onChange={(e) => props.handleInputChange(e.target.value)} />
+          </div>
+        </div>      
+    	)}
+  }
+
+  export default ListItem;
